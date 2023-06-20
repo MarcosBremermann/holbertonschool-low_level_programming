ec0efaee785f15dc32078e1710c79d50a39bcb6c
MarcosBremermann <mabm2004@gmail.com>
2023-06-20 09:47:37 -0700

    test6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - absolute value of an integrer
+ *
+ * @n: number variable
+ *
+ * Return: n or -n
+ *
+ */
+int _abs(int n)
+{
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+
+}
