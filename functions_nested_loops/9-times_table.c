9b9d61fa2115775b631115409a2c6c3578bf1786
MarcosBremermann <mabm2004@gmail.com>
2023-06-20 13:34:28 -0700

    test9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table - 9 tables
+ *
+ *
+ * Return: always zero
+ *
+ */
+void times_table(void)
+{
+	int row, col;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			int res = row * col;
+
+			_putchar("%d", res);
+		}
+		col = 0;
+		_putchar("\n");
+	}
+
+}
