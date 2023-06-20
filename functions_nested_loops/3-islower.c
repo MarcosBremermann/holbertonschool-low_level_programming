caa44c5a9b684725aa7498c0efe1d82f10ef6258
MarcosBremermann <mabm2004@gmail.com>
2023-06-20 06:52:05 -0700

    test33

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - check if c is lowercase
+ * @c: character
+ * Return: 1 if lowercase 0 else
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
