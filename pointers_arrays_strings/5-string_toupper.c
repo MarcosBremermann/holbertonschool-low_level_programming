367cb405308ee185126e3b261de7e65951dee1b6
MarcosBremermann <mabm2004@gmail.com>
2023-06-27 20:04:53 -0700

    Task 5 v1.0

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase to uppercase
+ *
+ * @str: string
+ *
+ * Return: returns string
+ *
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - 32;
+		}
+		ptr++;
+	}
+
+	return (str);
+}
