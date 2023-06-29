2b2d8e9bb170a2af95574bd3bffe4a8700c4c73e
MarcosBremermann <mabm2004@gmail.com>
2023-06-29 06:10:50 -0700

    Task 4 v1.0

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that powers x to y
+ *
+ * @x: base number
+ *
+ * @y: exponent number
+ *
+ * Return: when y < 0 is -1 in any other case is x^y
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else
+	{
+		return (x ^ y);
+	}
+}
