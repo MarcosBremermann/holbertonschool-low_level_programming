d2976a331e21b7e2df4df1f7c4e1fe405634d358
MarcosBremermann <mabm2004@gmail.com>
2023-06-29 07:27:21 -0700

    Task 5 v1.0

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - function that returns natural square root
+ *
+ * @n: number
+ *
+ * Return: square root of n
+ *
+ */
+int _sqrt_recursion(int n)
+{
+	static int i = 1;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	if (i * i == n)
+	{
+		return (i);
+	}
+
+	if (i * i > n)
+	{
+		return (-1);
+	}
+
+	i++;
+	return (_sqrt_recursion(n));
+}
