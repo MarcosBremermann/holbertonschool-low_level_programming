5fc4ed6fbda73da1fc4e8d2f10d252d10c8d86f1
MarcosBremermann <mabm2004@gmail.com>
2023-07-06 09:41:09 -0700

    Task 2 v1.01: added checks to handle when s1 or s2 are NULL

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,11 +14,18 @@ char *str_concat(char *s1, char *s2)
 	char *concatenated;
 	int len1 = 0, len2 = 0, i, j;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (s1 && s1[len1])
 	{
 		len1++;
 	}
-
 	while (s2 && s2[len2])
 	{
 		len2++;
