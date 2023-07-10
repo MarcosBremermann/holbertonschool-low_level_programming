7f7d93ef947c7c986574bda3c3cd6b635c4f2fde
MarcosBremermann <mabm2004@gmail.com>
2023-07-10 13:08:16 -0700

    Task 0 v1.0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents a dog with a name, age and owner.
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner's dog name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
