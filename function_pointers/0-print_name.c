e960e9ebbb152a1aa2cd365dd718d4ded9131db7
MarcosBremermann <mabm2004@gmail.com>
2023-07-11 10:08:32 -0700

    Task 0 v1.0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_name - function that prints name
+ *
+ * @name: name to be printed
+ *
+ * @f: function pointer for printing a single character
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		while (*name != '\0')
+		{
+			f(&(*name));
+			name++;
+		}
+	}
+}
