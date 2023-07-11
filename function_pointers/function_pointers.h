0d25448cdd189b5ee59958c3a581c11b30bbd256
MarcosBremermann <mabm2004@gmail.com>
2023-07-11 16:38:54 -0700

    Task 0 changed function_pointers

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-void print_name(const char* name, void (*modifier)(const char*));
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
 
 #endif /* FUNCTION_POINTERS_H */
