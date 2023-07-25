2f45ebc41f401749934ea25b56cc5e03cd6fe906
MarcosBremermann <mabm2004@gmail.com>
2023-07-25 12:04:52 -0700

    Task 0 v1.0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_list - function that prints element of list
+ *
+ * @h: pointer to the head of the linked list
+ *
+ * Return: number of nodes (node_count)
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] (nil)\n", 0);
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		node_count++;
+		h = h->next;
+	}
+
+	return (node_count);
+}
