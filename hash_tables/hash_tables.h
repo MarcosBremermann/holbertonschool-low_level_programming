64b406453d67927d5818c369da06b8539b9f7ffe
MarcosBremermann <mabm2004@gmail.com>
2023-08-02 12:55:49 -0700

    Task 0 v1.0

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,36 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+
+/*structures*/
+
+/**
+ * struct hash_node_s - node of hash table
+ * @key: key string
+ * @value: value associated with the key
+ * @next: pointer to next node
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - hash table data structure
+ * @size: size of array
+ * @array: pointer to the array of pointers to hash_node_t
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+/*prototypes*/
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
